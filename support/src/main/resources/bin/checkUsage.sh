bca689a8461b0d9aaf1ca6d2792213b01953ecf4
Chen Li <cli@cellossoftware.com>
2017-11-15 17:29:00 +1100

    fix script run warnnings

--- support/src/main/resources/bin/checkUsage.sh
+++ support/src/main/resources/bin/checkUsage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-exec scala "$0" "$@"
+SCALA=`which scala`
+exec $SCALA -cp ../libs/* "$0" "$@"
 !#
 object HelloWorld {
   def main(args: Array[String]) {
     println("Hello, world! " + args.toList)
   }
 }
-HelloWorld.main(args)
